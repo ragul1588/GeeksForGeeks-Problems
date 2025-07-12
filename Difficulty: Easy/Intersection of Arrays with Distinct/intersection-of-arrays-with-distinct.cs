2d0e5d7ae51c7084ddf9a3ca25d21bac4d0f9a9a
Ragul Iyappan <30984078+ragul1588@users.noreply.github.com>
2025-07-12 15:51:22 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Intersection of Arrays with Distinct/intersection-of-arrays-with-distinct.cs
+++ Difficulty: Easy/Intersection of Arrays with Distinct/intersection-of-arrays-with-distinct.cs
@@ -4,15 +4,12 @@ class Solution {
     // Function to return the count of the number of elements in
     // the intersection of two arrays.
     public int numberofElementsInIntersection(int[] a, int[] b) {
-         HashSet<int> set = new HashSet<int>();
+         HashSet<int> set = new HashSet<int>(a);
          int count = 0;
-        foreach(int val in a) set.Add(val);
-        
         foreach(int val in b) {
-           if(set.Contains(val)){
-               count++;
-           }   
+          if(set.Contains(val)) count++;   
         }
+        
         return count;
     }
 }
